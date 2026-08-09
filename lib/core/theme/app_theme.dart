bcfc3459e9774ce90690207c86d054bab24c0189
asaerxd6767 <mohammedasser070@gmail.com>
2026-08-10 01:23:24 +0300

    Refactor theme implementation by removing AppTheme class and directly using lightTheme

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:leoclinic_flutter/core/constants/app_colors.dart';
 
-class AppTheme {
-
-  ThemeData lightTheme = ThemeData(
-    scaffoldBackgroundColor: AppColors.background
-    
-  );
-}
+ThemeData lightTheme = ThemeData(
+  scaffoldBackgroundColor: AppColors.background,
+  fontFamily: 'IBMPlexSans',
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:leoclinic_flutter/core/utils/navbar.dart';
 import 'package:leoclinic_flutter/core/theme/app_theme.dart';
-
 import 'package:leoclinic_flutter/features/patient/presentation/pages/patient_info_page.dart';
 
 void main() {
@@ -15,7 +14,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: AppTheme().lightTheme,
+      theme: lightTheme,
       themeMode: ThemeMode.light,
       home: Scaffold(
         body: PatientInfoPage(),
